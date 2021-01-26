5ad17233d236323e1f3748ede8f43d79a1a10bab
Matheus Vieira <matheus.v4200@gmail.com>
2021-01-25 21:15:54 -0300

    estruturra inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-function Home(){
-	return <div>Home</div>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/global.module.css'
+import NavBar from '../components/navBar';
+import Footer from '../components/footer';
+
+function Home() {
+	return (
+		<div className={styles.body}>
+			<NavBar />
+			<div>
+				<img src="/images/HomePage/(1) Header.png" width="100%" />
+			</div>
+			<Footer>
+				footer
+			</Footer>
+		</div>
+	)
 }
 
 export default Home;
