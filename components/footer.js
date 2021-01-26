19948c18459edd2587e81773efab35d405dcfa2b
Matheus Vieira <matheus.v4200@gmail.com>
2021-01-26 20:06:59 -0300

    footer que contrai

--- components/footer.js
+++ components/footer.js
@@ -1,39 +1,38 @@
-import styles from '../styles/footer.css'
+import '../styles/footer.css'
 
 function Footer() {
 
-  
+
 
 	return (
 		<div className="footer">
-			<div className="row pr-5 pl-5 mr-5 ml-5">
-				<div className="col row ">
-					<a className="navbar-brand" href="#" >
-						<img src="/images/NavigationBar/logo.png" width="195" height="104" />
-					</a>
-					<div className="col p-3">
-						<span className="row">© 2020 Zenith Aerospace</span>
-						<span className="row">Avenida Trabalhador São-Carlense, 400, São Carlos, SP</span>
-						<span className="row">zenith.eesc@gmail.com</span>
-					</div>
+			<div className="footer--leftSide">
+				<a className="" href="#" >
+					<img src="/images/NavigationBar/logo.png" width="195" height="104" />
+				</a>
+
+				<div className="footer--data">
+					<span >© 2020 Zenith Aerospace</span>
+					<span >Avenida Trabalhador São-Carlense, 400, São Carlos, SP</span>
+					<span >zenith.eesc@gmail.com</span>
 				</div>
-				<div className="col-2 d-flex justify-content-between align-items-center">
-					<a className={styles.socialIconContainer} href="https://www.facebook.com/zenitheesc">
-						<img className="socialIcon" src="/images/Footer/facebook.png" width="28" height="28" />
-					</a>
-					<a className={styles.socialIconContainer} href="https://www.instagram.com/zenith_eesc/">
-						<img className="socialIcon" src="/images/Footer/Instagram.png" width="28" height="28" />
-					</a>
+			</div>
 
-					<a className={styles.socialIconContainer} href="https://github.com/zenitheesc">
-						<img className="socialIcon" src="/images/Footer/GitHub-Mark/PNG/GitHub-Mark-32px.png" width="28" height="28" />
-					</a>
+			<div>
+				<button className="socialIconContainer" href="https://www.facebook.com/zenitheesc">
+					<img className="socialIcon" src="/images/Footer/facebook.png" />
+				</button>
+				<button className="socialIconContainer" href="https://www.instagram.com/zenith_eesc/">
+					<img className="socialIcon" src="/images/Footer/Instagram.png" />
+				</button>
 
-					<a  className={styles.socialIconContainer} href="https://www.linkedin.com/company/zenith-aerospace/">
-						<img className="socialIcon" src="/images/Footer/LinkedIn.png" width="28" height="28" />
-					</a>
+				<button className="socialIconContainer" href="https://github.com/zenitheesc">
+					<img className="socialIcon" src="/images/Footer/GitHub-Mark/PNG/GitHub-Mark-32px.png" />
+				</button>
 
-				</div>
+				<button className="socialIconContainer" href="https://www.linkedin.com/company/zenith-aerospace/">
+					<img className="socialIcon" src="/images/Footer/LinkedIn.png" />
+				</button>
 			</div>
 
 		</div>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
-import styles from '../styles/global.css'
+import '../styles/global.css'
 import NavBar from '../components/navBar';
 import Footer from '../components/footer';
 
 function Home() {
 	return (
-		<div className={styles.body}>
+		<div>
 			<NavBar />
 			<div>
-				<img src="/images/HomePage/(1) Header.png" width="100%" />
+				<img src="/images/HomePage/(1) Header.png" width="100%" style={{display: "flex"}}/>
 			</div>
+			<Footer/>
 		</div>
 	)
 }
