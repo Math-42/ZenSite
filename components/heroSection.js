939ea507b84f44a2b646d4c64b10e9adad8590b8
mairacanal <maira.canal@usp.br>
2021-01-29 16:49:54 -0300

    Fixing videos

--- components/heroSection.js
+++ components/heroSection.js
@@ -4,7 +4,7 @@ import '../styles/heroSection.css'
 function HeroSection() {
     return (
         <div className='heroContainer'>
-            <video src='video2.mp4' autoPlay loop muted />
+            <video src='images/HomePage/video2.mp4' autoPlay loop muted />
             <h1>A CASA DAS ESTRELAS EM SÃO CARLOS</h1>
             <hr />
             <p>Desde 2014, o Zenith Aerospace proporciona a centenas de alunos a oportunidade de conquistar o espaço</p>
